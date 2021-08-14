b934c4ee129282526b4d3b13a5103786e6511cd4
Tarasov Artyom <tarasovar457@gmail.com>
2021-08-14 14:05:19 +0500

    add first lifecycle method in Users component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,60 @@
 import axios from "axios";
 import React from "react";
 import s from "./Users.module.css";
-import userPhoto from "../../assets/images/no_avatar.png"
-const Users = (props) => {
-  if (props.users.length === 0) {
+import userPhoto from "../../assets/images/no_avatar.png";
+
+class Users extends React.Component {
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
-        console.log(response.data.items)
-        props.setUsers(response.data.items);
+        this.props.setUsers(response.data.items);
       });
   }
-
-  return (
-    <div className={s.content}>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img alt="ava" src={u.photos.small != null ? u.photos.small : userPhoto} className={s.ava} />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id);
-                  }}
-                >
-                  UNFOLLOW
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  FOLLOW
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+  render() {
+    return (
+      <div className={s.content}>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
             <span>
-              <div>shortname: {u.name}</div>
-              <div>{u.status != null ? "status:" + u.status : ""}</div>
+              <div>
+                <img
+                  alt="ava"
+                  src={u.photos.small != null ? u.photos.small : userPhoto}
+                  className={s.ava}
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id);
+                    }}
+                  >
+                    UNFOLLOW
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id);
+                    }}
+                  >
+                    FOLLOW
+                  </button>
+                )}
+              </div>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  );
-};
+            <span>
+              <span>
+                <div>shortname: {u.name}</div>
+                <div>{u.status != null ? "status:" + u.status : ""}</div>
+              </span>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
 export default Users;
